b7b4e5418ba2eabb6e8f2efebe2f2282f1538ace
LeoPopolo <leopopologuitarreta@gmail.com>
2017-02-08 10:21:58 -0300

    if ejercicio 4

---
+++ jsInstruccionIF (4).js
@@ -0,0 +1,12 @@
+function Mostrar()
+{
+//tomo la edad  
+
+var edad;
+  edad=document.getElementById('edad').value;
+  edad=parseInt(edad);
+  if(edad>=13 && edad<=17) {
+  	alert("usted es adolescente");
+  }
+
+}//FIN DE LA FUNCIÓN
