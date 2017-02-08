4df107c95bef706bac5a131cd2841b7ae6e676be
LeoPopolo <leopopologuitarreta@gmail.com>
2017-02-08 11:52:15 -0300

    if ejercicio 7

---
+++ jsInstruccionIF (7).js
@@ -0,0 +1,14 @@
+function Mostrar()
+{
+//tomo la edad  
+
+	var edad;
+	var estadoCivil;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+	estadoCivil= document.getElementById('estadoCivil').value;
+    if(edad<=18 && estadoCivil !="soltero"){
+    	document.write("es muy pequeño para no ser soltero");
+    }
+
+}//FIN DE LA FUNCIÓN
