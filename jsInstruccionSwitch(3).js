3839e1ef044bb10bdba2d1e755cfd6934920a4db
LeoPopolo <leopopologuitarreta@gmail.com>
2017-02-09 11:46:50 -0300

    switch ejercicio 3

---
+++ jsInstruccionSwitch(3).js
@@ -0,0 +1,27 @@
+function Mostrar()
+{
+var mesDelAño = document.getElementById('mes').value;
+	switch(mesDelAño)
+		{
+			case "Febrero":
+			alert("este mes no tiene mas de 29 dias");
+			break;
+
+			default:
+			 alert("este mes tiene 30 o mas dias");
+
+			/*case "Abril":
+			case "Junio":
+			case "Septiembre":
+			case "Noviembre":
+			alert("este mes tiene 30 o mas dias");
+			break;*/
+
+
+		}
+
+	
+	
+
+
+}//FIN DE LA FUNCIÓN
