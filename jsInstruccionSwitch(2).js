c62c085eacf579cf1d572f0f35a1ee8c231b9688
LeoPopolo <leopopologuitarreta@gmail.com>
2017-02-09 11:26:46 -0300

    switch ejercicio 2

---
+++ jsInstruccionSwitch(2).js
@@ -0,0 +1,29 @@
+function Mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+//alert (mesDelAño);
+	switch(mesDelAño)
+	{
+		case "Enero":
+		case "Febrero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "junio":
+			alert("falta para el invierno");
+			break;
+		case "Julio":
+		case "Agosto":
+			alert("abrigate que hace frio");
+			break;
+		case "Septiembre":
+		case "Octubre":
+		case "Noviembre":
+ 		case "Diciembre":
+ 			alert("ya pasamos el frio, ahora calor !!!");
+ 			break;
+
+    }
+}//FIN DE LA FUNCIÓN
